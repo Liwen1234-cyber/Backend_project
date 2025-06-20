63c0d3f8127927bb081d9353facc3022f56bb2b8
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2025-06-20 20:10:26 +0800

    redis完结

--- Comments/redis-main/Mycode/hm-dianping/src/main/java/com/hmdp/service/IShopService.java
+++ Comments/redis-main/Mycode/hm-dianping/src/main/java/com/hmdp/service/IShopService.java
@@ -13,8 +13,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-12-22
  */
 public interface IShopService extends IService<Shop> {
-
     Result queryById(Long id);
 
     Result update(Shop shop);
+
+    Result queryShopByType(Integer typeId, Integer current, Double x, Double y);
+
 }
--- Comments/redis-main/Mycode/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ Comments/redis-main/Mycode/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -9,14 +9,22 @@ import org.junit.jupiter.api.Test;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
 
 
 @SpringBootTest
@@ -78,5 +86,55 @@ public class HmDianPingApplicationTests {
                 }//这里为什么还要再判断一次，在5.7中讲解
             }
         }
+    }
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Test
+    void loadShopData() {
+        // 1.查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2.把店铺分组，按照typeId分组，typeId一致的放到一个集合
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3.分批完成写入Redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 3.1.获取类型id
+            Long typeId = entry.getKey();
+            String key = SHOP_GEO_KEY + typeId;
+            // 3.2.获取同类型的店铺的集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            // 3.3.写入redis GEOADD key 经度 纬度 member
+            for (Shop shop : value) {
+                // stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
+    }
+
+    @Test
+    void testHyperLogLog() {
+        //准备数组，装用户数据
+        String[] users = new String[1000];
+        //数组角标
+        int index = 0;
+        for (int i = 1; i <= 1000000; i++) {
+
+            users[index++] = "user_" + i;
+            //每1000条发送一次
+            if (i % 1000 == 0) {
+                index = 0;
+                stringRedisTemplate.opsForHyperLogLog().add("hll1", users);
+                //统计数量
+
+            }
+        }
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("hll1");
+        System.out.println("size =" + size);
     }
 }
