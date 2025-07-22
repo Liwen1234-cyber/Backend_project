063debfd232fc78bdb28a988f9d77894801e2650
Liwen1234-cyber <175311843+Liwen1234-cyber@users.noreply.github.com>
2025-07-22 20:41:44 +0800

    完成黑马LangChain4j

---
+++ Langchain4j/consultant/src/main/java/cn/itheima/consultant/aiservice/ConsultantService.java
@@ -0,0 +1,28 @@
+package cn.itheima.consultant.aiservice;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.service.spring.AiServiceWiringMode;
+import reactor.core.publisher.Flux;
+
+@AiService(
+        wiringMode = AiServiceWiringMode.EXPLICIT,//手动装配
+        chatModel = "openAiChatModel",//指定模型
+        streamingChatModel = "openAiStreamingChatModel",
+//        chatMemory = "chatMemory",//配置会话记忆对象
+        chatMemoryProvider = "chatMemoryProvider",//配置会话记忆提供者对象
+        contentRetriever = "contentRetriever",//配置向量数据库检索对象
+        tools = "reservationTool"
+)
+public interface ConsultantService {
+//    @SystemMessage("你是东哥的助手小月，人美心善又多金！")
+//    public Flux<String> chat(String message);
+
+
+    @SystemMessage(fromResource = "system.txt")
+//    @UserMessage("你是东哥的助手小月，人美心善又多金！{{msg}}")
+    public Flux<String> chat(@MemoryId String memoryId,@UserMessage String message);
+}
